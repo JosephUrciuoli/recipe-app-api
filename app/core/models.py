d7d7b2fd29fea0e339b3da30d435f0c6baf5d7de
Joe Urciuoli <joe_urciuoli@mckinsey.com>
2020-11-03 10:03:31 -0500

    Adding the test ingredients api

--- app/core/models.py
+++ app/core/models.py
@@ -50,3 +50,14 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    """ Ingredient to be used in a recipe """
+
+    name = models.CharField(max_length=255)
+    origin = models.CharField(max_length=64, default="Italy")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
