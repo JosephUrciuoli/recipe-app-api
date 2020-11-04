3503c8aa0a2909a92c980704341a4767d61ef011
Joe Urciuoli <joe_urciuoli@mckinsey.com>
2020-11-04 11:29:06 -0500

    "adding tests for images

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,5 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -5,6 +7,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """ generate file path for new recipe image """
+    ext = filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+
+    return os.path.join("uploads/recipe/", filename)
 
 
 class UserManager(BaseUserManager):
@@ -73,6 +83,7 @@ class Recipe(models.Model):
     ingredients = models.ManyToManyField("Ingredient")
     tags = models.ManyToManyField("Tag")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
